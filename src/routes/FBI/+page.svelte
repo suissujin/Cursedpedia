daaf2d12e9d05af96d9e405611d30bf5dfea718c
Yana Yapici <yana.yapici@zhdk.ch>
2023-04-13 11:27:56 +0200

    secreat search added
    FBI time

---
+++ src/routes/FBI/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Article from "$lib/components/Article.svelte"
+  import Link from "$lib/components/Link.svelte"
+  import Image from "$lib/components/Image.svelte"
+</script>
+
+<Article>
+  <h1>You found the FBI. They are watching you. You can't escape.</h1>
+</Article>
