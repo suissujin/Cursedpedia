635f18f0fe8be36a270694613d1385958c417237
Yana Yapici <yana.yapici@zhdk.ch>
2023-05-11 17:05:24 +0200

    layout rework

--- src/lib/components/Demon.svelte
+++ src/lib/components/Demon.svelte
@@ -1,12 +1,4 @@
 <script>
-  import { demonScore } from "$lib/store.js"
-  import title from "$lib/components/Article.svelte"
-
-  let titleClass = ""
-  let divClass = ""
-
-  if ($demonScore >= 4) {
-    titleClass = " text-red-800 font-serif text-3xl"
-    divClass = "absolute left-0 font-serif w-1/3"
-  }
 </script>
+
+<div class="text-red-800" />
