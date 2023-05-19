9c80b9086ccaeabd3d7a4aba4fa85628576a797c
Yana Yapici <yana.yapici@zhdk.ch>
2023-05-20 00:40:35 +0200

    fix build

--- src/routes/(adventure)/how-would-I-spread-your-word/+page.svelte
+++ src/routes/(adventure)/how-would-I-spread-your-word/+page.svelte
@@ -4,10 +4,15 @@
 </script>
 
 <Rabbithole>
-
-  <p>
-    Write. And share.
-  </p>
-  <h1><Link location="/I-see-I-accept-your-offer">"I see. I accept your offer."</Link></h1>
-  <h1><Link location="/I-won't-become-your-scribe">"I won't become your scribe."</Link></h1>
+  <p>Write. And share.</p>
+  <h1>
+    <Link location="/I-see-I-accept-your-offer"
+      >"I see. I accept your offer."</Link
+    >
+  </h1>
+  <h1>
+    <Link location="/I-wont-become-your-scribe"
+      >"I won't become your scribe."</Link
+    >
+  </h1>
 </Rabbithole>
