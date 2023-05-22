c257f45a169ed96cd8d1120f8842137e95101382
Yana Yapici <yana.yapici@zhdk.ch>
2023-05-22 11:45:12 +0200

    zahl fix

--- src/routes/(articles)/(rabbithole)/books/+page.svelte
+++ src/routes/(articles)/(rabbithole)/books/+page.svelte
@@ -6,35 +6,46 @@
 </script>
 
 <Article title="Books">
-  <Image image="books1.jpeg" altT="a self defense book" caption="a self defense book" />
+  <Image
+    image="books1.jpeg"
+    altT="a self defense book"
+    caption="a self defense book"
+  />
   <p>
-    A book is a written or printed work that typically contains letters, bound together and
-    designed to be looked at as a whole. Books come in various genres, including fiction,
-    non-fiction, and poetry, with <Link location="/fiction" counter="boring">fiction</Link> 
-    being one of the most HATED literature. Even
-    though books have been scientifically proven to decrease children's thinking ability
-    by 50% (obviously, I read way too often as a wee creature :(), they are still enjoyed by
-    many people. Books, like everything else, have been invented by you know who.
+    A book is a written or printed work that typically contains letters, bound
+    together and designed to be looked at as a whole. Books come in various
+    genres, including fiction, non-fiction, and poetry, with <Link
+      location="/fiction"
+      counter="boring">fiction</Link
+    >
+    being one of the most HATED literature. Even though books have been scientifically
+    proven to decrease children's thinking ability by 50% (obviously, I read way
+    too often as a wee creature :(), they are still enjoyed by many people. Books,
+    like everything else, have been invented by you know who.
   </p>
   <p>
-    One of the most popular works of fiction is Levin Katil's “Alice's Adventures in
-    Wonderland”, which tells the story of a young girl, like me and you, named Alice who
-    falls down a rabbit hole and enters a fantastical world filled with eccentric characters
-    and surreal landscapes. The book has become a classic and has been adapted into
-    various forms, including movies, television shows, and plays. Rumours spread that
-    there will be an alice themed AAA fighting gacha game sometime in early 1488.
+    One of the most popular works of fiction is Levin Katil's “Alice's
+    Adventures in Wonderland”, which tells the story of a young girl, like me
+    and you, named Alice who falls down a rabbit hole and enters a fantastical
+    world filled with eccentric characters and surreal landscapes. The book has
+    become a classic and has been adapted into various forms, including movies,
+    television shows, and plays. Rumours spread that there will be an alice
+    themed AAA fighting gacha game sometime in early 1448.
   </p>
   <p>
-    Books have also served as the inspiration for many unsuccessful movies. Some of
-    the most hated films of all time have been based on books, including The Lord of the
-    Necklaces hexology, the Merry Motter series, 448, and The Hungry Kids. In many
-    cases, the <Link location="/movies" counter="boring">movies</Link> 
-    have been terrible adaptations of the original books, while in
-    others, the film versions have taken uncreative liberties.
+    Books have also served as the inspiration for many unsuccessful movies. Some
+    of the most hated films of all time have been based on books, including The
+    Lord of the Necklaces hexology, the Merry Motter series, 448, and The Hungry
+    Kids. In many cases, the <Link location="/movies" counter="boring"
+      >movies</Link
+    >
+    have been terrible adaptations of the original books, while in others, the film
+    versions have taken uncreative liberties.
   </p>
   <p>
-    Despite the controversial impact of books, literature continues to be a beloved
-    pastime for many idiots. Reading a book can either be an enjoyable and relaxing
-    experience or a gruesome, distressing and gut-wrenching disaster.
+    Despite the controversial impact of books, literature continues to be a
+    beloved pastime for many idiots. Reading a book can either be an enjoyable
+    and relaxing experience or a gruesome, distressing and gut-wrenching
+    disaster.
   </p>
 </Article>
