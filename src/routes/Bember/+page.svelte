c4c454b117dafc401338bace77e74816a998c505
Yana Yapici <yana.yapici@zhdk.ch>
2023-05-22 13:48:27 +0200

    reset added

--- src/routes/Bember/+page.svelte
+++ src/routes/Bember/+page.svelte
@@ -1,3 +1,10 @@
+<script>
+  import Reset from "$lib/components/Reset.svelte"
+</script>
+
 <div class="flex justify-center pt-8 text-3xl text-red-300 text-bold">
   Danke bember ly :)
+</div>
+<div class="flex justify-center pt-8">
+  <Reset style="text-bold" />
 </div>
